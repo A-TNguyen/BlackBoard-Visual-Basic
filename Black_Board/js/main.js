6c597f6a94529d9b13cf333a00cd1701329a2e4f
krazyforce <40051025+krazyforce@users.noreply.github.com>
2019-10-14 13:15:53 -0500

    Javascript integration
    Adding JS to the code

---
+++ Black_Board/js/main.js
@@ -0,0 +1,9 @@
+let name;
+
+name = window.prompt("Please enter your name");
+
+if (name) {
+  for (let i = 0; i < 1; i++) {
+    document.writeln("<h1> Welcome to the page " + name, "</h1>");
+  }
+}
