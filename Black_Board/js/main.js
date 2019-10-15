fa504ca2a1e8b3f9aef9d9285ab08515b9f7618f
krazyforce <40051025+krazyforce@users.noreply.github.com>
2019-10-15 12:52:45 -0500

    JS

--- Black_Board/js/main.js
+++ Black_Board/js/main.js
@@ -1,9 +1,10 @@
 let name;
+let size = 10;
 
 name = window.prompt("Please enter your name");
 
 if (name) {
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < size; i++) {
     document.writeln("<h1> Welcome to the page " + name, "</h1>");
   }
 }
